913a4d293ef7513f373262cdff98bd9a4a7a9720
Fadi <mfd4951@rit.edu>
2023-09-11 15:18:33 +0400

    Colors and Functionality +

--- turtle stuff.py
+++ turtle stuff.py
@@ -9,12 +9,24 @@
 # turtle.isdown() prints true if pen is down, and false if its up
 # turtle.heading( returns the current angle of orientatio nof the turtle
 # turtle.xcor(), turtle.ycor() return x and y coordinates of the turtle
+# turtle.pencolor() for color
+# turtle.pensize(size) for size, turtle.fillcolor(color), turtle.begin_fill, turtle.end_fill()
 
 
 
 import turtle as t
 
+def turtle_state():
+    updown = t.isdown()
+    heading = t.heading()
+    xcor = t.xcor()
+    ycor = t.ycor()
+    print("Pen Status :",updown,"\nHeading :",heading, "\nThe coordinates are\nX:",xcor,"\nY:",ycor)
+
 def testdrive():
+
+    t.pencolor("white")
+
     t.forward(100)
     t.left(87)
     t.setheading(127)
@@ -27,30 +39,53 @@ def testdrive():
     t.home()
     t.circle(25)
 
-def turtle_state():
-    updown = t.isdown()
-    heading = t.heading()
-    xcor = t.xcor()
-    ycor = t.ycor()
-    print("Pen Status :",updown,"\nHeading :",heading, "\nThe coordinates are\nX:",xcor,"\nY:",ycor)
 
-def square():
+parametX = int(input("Pick the X scale for the square!"))
+parametY = int(input("Pick the Y scale for the square!"))
+parametH = int(input("Select the heading!"))
+
+
+def square(a, b, c):
     t.up()
     t.goto(100,100)
     t.down()
-    t.forward(90)
-    t.left(90)
-    t.forward(90)
-    t.left(90)
-    t.forward(90)
-    t.left(90)
-    t.forward(90)
+
+    t.setheading(c)
+
+    t.pensize(4)
+    t.pencolor("red")
+    t.fillcolor("blue")
+
+
+    t.delay(40)
+    t.forward(a)
+    t.left(b)
+    t.forward(a)
+    t.left(b)
+
+    t.begin_fill()
+
+    t.forward(a)
+    t.left(b)
+    t.forward(a)
+
+    t.end_fill()
+
 
 def main():
+    t.bgcolor("black")
+
     testdrive()
-    square()
+    #square(parametX, parametY, parametH)
+
+    t.goto(80,80)
+
+    square(90, 90, 180)
+    square(90, 90, 280)
+    square(90, 90, 380)
+
+
     turtle_state()
     input("Press enter to continue . . .")
 
 main()
-
